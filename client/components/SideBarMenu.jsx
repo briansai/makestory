02fd49b33f1945b2ef163b7f50adfc983f48a93d
Brian Sai <brianwsai@gmail.com>
2021-03-22 13:53:29 -0700

    Created a responsive top navbar when screen width changes

--- client/components/SideBarMenu.jsx
+++ client/components/SideBarMenu.jsx
@@ -1,15 +1,19 @@
-import { Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const SideBarMenu = () => {
   return (
-    <Nav className="flex-column bg-dark sidebar" defaultActiveKey="/home">
-      <Nav.Link eventKey="link-1" className="text-white text-center link">
-        <div>
-          <i width="32" height="32" className="bi bi-journal-text" />
-        </div>
-        <div>Stories</div>
-      </Nav.Link>
-    </Nav>
+    <Navbar className="side-navbar">
+      <Navbar.Collapse>
+        <Nav className="flex-column bg-dark sidebar" defaultActiveKey="/home">
+          <Nav.Link eventKey="link-1" className="text-white text-center link">
+            <div>
+              <i width="32" height="32" className="bi bi-journal-text" />
+            </div>
+            <div>Stories</div>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
--- client/components/TopMenu.jsx
+++ client/components/TopMenu.jsx
@@ -1,17 +1,21 @@
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
 
-const TopMenu = () => (
-  <Navbar bg="dark" variant="dark" className="shadow nav">
-    <Navbar.Brand href="#home">MakeStory</Navbar.Brand>
-    <Form inline className="mx-auto">
-      <FormControl
-        type="text"
-        placeholder="Search"
-        className="mr-sm-2 search"
-      />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-  </Navbar>
-);
+const TopMenu = () => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark" className="shadow nav">
+      <Navbar.Brand href="#home" className="brand">
+        {isTabletOrMobile ? 'M' : 'MakeStory'}
+      </Navbar.Brand>
+      <Form className="mx-auto search-container">
+        <FormControl type="text" placeholder="Search" className="sm-2 search" />
+        <Button variant="outline-light" className="search-button">
+          <i className="bi bi-search"></i>
+        </Button>
+      </Form>
+    </Navbar>
+  );
+};
 
 export default TopMenu;
